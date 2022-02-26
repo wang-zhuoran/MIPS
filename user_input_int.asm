634bb6476b7b63584ff841893543f32be54c69c2
ZhuoranWang <41529680+wang-zhuoran@users.noreply.github.com>
2022-02-26 16:24:25 +0800

    Add files via upload

---
+++ user_input_int.asm
@@ -0,0 +1,37 @@
+.data
+	prompt: .asciiz "Enter your age: "
+	message: .asciiz "\nYour age is "
+	
+.text
+	# Prompt the user to enter age
+	li $v0, 4
+	la $a0, prompt
+	syscall
+	
+	# Get the user's age
+	li $v0, 5 # input an integer
+	syscall
+	
+	# Store the age in $t0
+	add $t0, $zero, $v0
+	
+	# Display message
+	li $v0, 4
+	la $a0, message
+	syscall 
+	
+	# Print the age
+	li $v0, 1
+	move $a0, $t0
+	syscall
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
